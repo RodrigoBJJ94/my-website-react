094c433345fd8ecff3ac0b70210a3147c6fac911
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-02 03:51:37 -0300

    Version 1.05

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import { default as HomeSvg } from './assets/img/HomeSvg.svg';
+import { default as MobileSvg } from './assets/img/MobileSvg.svg';
+import { default as GamesSvg } from './assets/img/GamesSvg.svg';
+import { default as ProjectsSvg } from './assets/img/ProjectsSvg.svg';
 
 export default function App() {
   return (
@@ -22,7 +25,7 @@ export default function App() {
       <section id="home" className="intro main-background section">
         <div className="main-content intro-content">
           <div className="intro-text-content">
-            <h2>Developer</h2>
+            <h2 className="home-and-contact">Developer</h2>
             <p className="tab">I'm a front-end and mobile developer, in addition I have knowledge in back-end and database</p>
             <p className="tab">I have intermediate knowledge in HTML, CSS and Javascript languages, React Native and ReactJS libraries and GitHub Desktop</p>
             <p className="tab">I have basic knowledge of Typescript and Python languages, MongoDB and PostgreSQL databases and NodeJS</p>
@@ -34,43 +37,58 @@ export default function App() {
       </section>
       <section id="mobile" className="white-background section apps-back">
         <div className="main-content grid-one-content mobile">
-          <h2>Mobile</h2>
-          <p className="grid-description">Below are my Android apps and games, developed in React Native</p>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Tic Tac Toe</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculator</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculator of IMC</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.gasolinaOuEtanol" target="_blank" rel="noreferrer">Gasolina or Ethanol</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.headsOrTailsCaraOuCoroa" target="_blank" rel="noreferrer">Heads or Tails</a></h4>
+          <div className="intro-text-content">
+            <h2>Mobile</h2>
+            <p className="grid-description">Below are my Android apps and games, developed in React Native</p>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Tic Tac Toe</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculator</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculator of IMC</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.gasolinaOuEtanol" target="_blank" rel="noreferrer">Gasolina or Ethanol</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.headsOrTailsCaraOuCoroa" target="_blank" rel="noreferrer">Heads or Tails</a></h4>
+          </div>
+          <div className="mobile-background-svg" >
+            <img src={MobileSvg} alt="" className="mobile-background-svg" />
+          </div>
         </div>
       </section>
       <section id="projects" className="grid-one main-background section">
-        <div className="main-content grid-one-content">
-          <h2>Projects</h2>
-          <p className="grid-description">Below are some of my projects</p>
-          <h4><a href="https://rodrigobrentano-pomodoro.netlify.app" target="_blank" rel="noreferrer">Pomodoro</a></h4>
-          <h4><a href="https://rodrigobrentano-calculator.netlify.app" target="_blank" rel="noreferrer">Calculator</a></h4>
-          <h4><a href="https://rodrigobrentano-calculadora-de-imc.netlify.app" target="_blank" rel="noreferrer">Calculator of BMI</a></h4>
-          <h4><a href="https://rodrigobrentano-lista-de-tarefas.netlify.app" target="_blank" rel="noreferrer">To-do List</a></h4>
-          <h4><a href="https://rodrigobrentano-relogio-digital.netlify.app" target="_blank" rel="noreferrer">Digital Watch</a></h4>
-          <h4><a href="https://rodrigobrentano-timer.netlify.app" target="_blank" rel="noreferrer">Timer</a></h4>
+        <div className="main-content grid-one-content projects-white">
+          <div className="intro-text-content">
+            <h2>Projects</h2>
+            <p className="grid-description">Below are some of my projects</p>
+            <h4><a href="https://rodrigobrentano-pomodoro.netlify.app" target="_blank" rel="noreferrer" >Pomodoro</a></h4>
+            <h4><a href="https://rodrigobrentano-calculator.netlify.app" target="_blank" rel="noreferrer" >Calculator</a></h4>
+            <h4><a href="https://rodrigobrentano-calculadora-de-imc.netlify.app" target="_blank" rel="noreferrer" >Calculator of BMI</a></h4>
+            <h4><a href="https://rodrigobrentano-lista-de-tarefas.netlify.app" target="_blank" rel="noreferrer" >To-do List</a></h4>
+            <h4><a href="https://rodrigobrentano-relogio-digital.netlify.app" target="_blank" rel="noreferrer" >Digital Watch</a></h4>
+            <h4><a href="https://rodrigobrentano-timer.netlify.app" target="_blank" rel="noreferrer" >Timer</a></h4>
+          </div>
+          <div className="projects-background-svg" >
+            <img src={ProjectsSvg} alt="" className="projects-background-svg" />
+          </div>
         </div>
       </section>
       <section id="games" className="grid-one white-background section back">
         <div className="main-content grid-one-content">
-          <h2 className="grid-main-heading">Games</h2>
-          <p className="grid-description">Below are some of my games</p>
-          <div className="games">
-            <h4><a href="https://rodrigobrentano-jogo-da-velha.netlify.app" target="_blank" rel="noreferrer">Tic Tac Toe - 2 Player</a></h4>
-            <h4><a href="https://rodrigobrentano-jogo-da-velha-versus-cpu.netlify.app" target="_blank" rel="noreferrer">Tic Tac Toe - vs CPU</a></h4>
-            <h4><a href="https://rodrigobrentano-ping-pong.netlify.app" target="_blank" rel="noreferrer">Ping Pong - for Computer</a></h4>
-            <h4><a href="https://rodrigo-brentano-jogo-da-nave.netlify.app" target="_blank" rel="noreferrer">Ship Game - for Computer</a></h4>
+          <div className="intro-text-content">
+            <h2>Games</h2>
+            <p className="grid-description">Below are some of my games</p>
+            <div className="games">
+              <h4><a href="https://rodrigobrentano-jogo-da-velha.netlify.app" target="_blank" rel="noreferrer">Tic Tac Toe - 2 Player</a></h4>
+              <h4><a href="https://rodrigobrentano-jogo-da-velha-versus-cpu.netlify.app" target="_blank" rel="noreferrer">Tic Tac Toe - vs CPU</a></h4>
+              <h4><a href="https://rodrigobrentano-ping-pong.netlify.app" target="_blank" rel="noreferrer">Ping Pong - for Computer</a></h4>
+              <h4><a href="https://rodrigo-brentano-jogo-da-nave.netlify.app" target="_blank" rel="noreferrer">Ship Game - for Computer</a></h4>
+            </div>
+          </div>
+          <div className="games-background-svg" >
+            <img src={GamesSvg} alt="" className="games-background-svg" />
           </div>
         </div>
       </section>
       <section id="contact" className="intro main-background section">
         <div className="main-content intro-contact">
           <div className="intro-text-content contact">
-            <h2>Contact</h2>
+            <h2 className="home-and-contact">Contact</h2>
             <p><strong>WhatsApp:</strong>  +55 51 992191122</p>
             <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/</a></p>
             <p><strong>Email:</strong> rodrigobjj24@gmail.com</p>
