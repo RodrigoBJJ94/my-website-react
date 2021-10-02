3a5304cedd3d52396d4b0a71e5a62a003672db0d
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-02 02:41:54 -0300

    Version 1.04

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
+import { default as HomeSvg } from './assets/img/HomeSvg.svg';
 
 export default function App() {
-  const [menu, setMenu] = useState(false);
-
   return (
     <>
       <aside className="menu white-background">
@@ -12,9 +10,9 @@ export default function App() {
           <h1><a href="#home">HOME</a></h1>
           <nav>
             <ul>
-            <li><a href="#projects">Projects</a></li>
-              <li><a href="#games">Games</a></li>
               <li><a href="#mobile">Mobile</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#games">Games</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
           </nav>
@@ -25,19 +23,30 @@ export default function App() {
         <div className="main-content intro-content">
           <div className="intro-text-content">
             <h2>Developer</h2>
-            <p className="tab">I'm a front-end and mobile developer, in addition I have knowledge in back-end and database.</p>
-            <p className="tab">I have intermediate knowledge in HTML, CSS and Javascript, React Native and ReactJS libraries and GitHub Desktop technology.</p>
-            <p className="tab">I have basic knowledge of Typescript and Python languages, MongoDB and PostgreSQL databases and NodeJS.</p>
+            <p className="tab">I'm a front-end and mobile developer, in addition I have knowledge in back-end and database</p>
+            <p className="tab">I have intermediate knowledge in HTML, CSS and Javascript languages, React Native and ReactJS libraries and GitHub Desktop</p>
+            <p className="tab">I have basic knowledge of Typescript and Python languages, MongoDB and PostgreSQL databases and NodeJS</p>
           </div>
-          <div className="intro-img2">
-            <img src="" alt="" />
+          <div className="main-background-svg" >
+            <img src={HomeSvg} alt="" className="main-background-svg" />
           </div>
         </div>
       </section>
-      <section id="projects" className="grid-one white-background section back">
+      <section id="mobile" className="white-background section apps-back">
+        <div className="main-content grid-one-content mobile">
+          <h2>Mobile</h2>
+          <p className="grid-description">Below are my Android apps and games, developed in React Native</p>
+          <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Tic Tac Toe</a></h4>
+          <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculator</a></h4>
+          <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculator of IMC</a></h4>
+          <h4><a href="https://play.google.com/store/apps/details?id=com.gasolinaOuEtanol" target="_blank" rel="noreferrer">Gasolina or Ethanol</a></h4>
+          <h4><a href="https://play.google.com/store/apps/details?id=com.headsOrTailsCaraOuCoroa" target="_blank" rel="noreferrer">Heads or Tails</a></h4>
+        </div>
+      </section>
+      <section id="projects" className="grid-one main-background section">
         <div className="main-content grid-one-content">
           <h2>Projects</h2>
-          <p>Below are some of my projects</p>
+          <p className="grid-description">Below are some of my projects</p>
           <h4><a href="https://rodrigobrentano-pomodoro.netlify.app" target="_blank" rel="noreferrer">Pomodoro</a></h4>
           <h4><a href="https://rodrigobrentano-calculator.netlify.app" target="_blank" rel="noreferrer">Calculator</a></h4>
           <h4><a href="https://rodrigobrentano-calculadora-de-imc.netlify.app" target="_blank" rel="noreferrer">Calculator of BMI</a></h4>
@@ -46,7 +55,7 @@ export default function App() {
           <h4><a href="https://rodrigobrentano-timer.netlify.app" target="_blank" rel="noreferrer">Timer</a></h4>
         </div>
       </section>
-      <section id="games" className="grid-one main-background section">
+      <section id="games" className="grid-one white-background section back">
         <div className="main-content grid-one-content">
           <h2 className="grid-main-heading">Games</h2>
           <p className="grid-description">Below are some of my games</p>
@@ -58,25 +67,14 @@ export default function App() {
           </div>
         </div>
       </section>
-      <section id="mobile" className="white-background section apps-back">
-        <div className="main-content grid-one-content">
-          <h2>Mobile</h2>
-          <p className="grid-description">Below are my Android apps and games, developed in React Native</p>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Tic Tac Toe</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculator</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculator of IMC</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.gasolinaOuEtanol" target="_blank" rel="noreferrer">Gasolina or Ethanol</a></h4>
-          <h4><a href="https://play.google.com/store/apps/details?id=com.headsOrTailsCaraOuCoroa" target="_blank" rel="noreferrer">Heads or Tails</a></h4>
-        </div>
-      </section>
       <section id="contact" className="intro main-background section">
-        <div className="main-content intro-content">
+        <div className="main-content intro-contact">
           <div className="intro-text-content contact">
             <h2>Contact</h2>
-            <p>WhatsApp: +55 51 992191122</p>
-            <p>LinkedIn: <a href="https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/</a></p>
-            <p>Email: rodrigobjj24@gmail.com</p>
-            <p>GitHub: <a href="https://github.com/RodrigoBJJ94" target="_blank" rel="noreferrer">https://github.com/RodrigoBJJ94</a></p>
+            <p><strong>WhatsApp:</strong>  +55 51 992191122</p>
+            <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/</a></p>
+            <p><strong>Email:</strong> rodrigobjj24@gmail.com</p>
+            <p><strong>GitHub:</strong> <a href="https://github.com/RodrigoBJJ94" target="_blank" rel="noreferrer">https://github.com/RodrigoBJJ94</a></p>
           </div>
         </div>
         <div>
