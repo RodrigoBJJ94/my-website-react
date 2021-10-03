8f644ec26a44d99b55998aaf601048c4c5b7daa6
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-02 22:53:49 -0300

    Version 1.11

--- src/components/Home/HomeMain.js
+++ src/components/Home/HomeMain.js
@@ -5,7 +5,7 @@ export default function HomeMain() {
         <>
             <p className="tab">I'm a front-end and mobile developer, in addition I have knowledge in back-end and database</p>
             <p className="tab">I have intermediate knowledge in HTML, CSS and Javascript languages, React Native and ReactJS libraries and GitHub Desktop</p>
-            <p className="tab">I have basic knowledge of Typescript and Python languages, MongoDB and PostgreSQL databases and NodeJS</p>
+            <p className="tab">I have basic knowledge in Typescript and Python languages, MongoDB and PostgreSQL databases, NodeJS and Git</p>
         </>
     );
 };
