6decc1f0f7278b7c54904107a4b629a9a72672e8
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-17 17:49:06 -0300

    Version 1.24

--- src/components/Contact/ContactMain.js
+++ src/components/Contact/ContactMain.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ContactMain() {
     return (
         <>
-            <p><strong>WhatsApp:</strong>  +55 51 992191122</p>
+            <p><strong>WhatsApp:</strong> <a href="https://api.whatsapp.com/send?phone=5551992191122" target="_blank" rel="noreferrer" className="contact-mobile" >+55 51 992191122</a></p>
             <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/" target="_blank" rel="noreferrer" className="contact-mobile" >...rodrigo-brentano-26a7ba204</a></p>
             <p><strong>Email:</strong> rodrigobjj24@gmail.com</p>
             <p><strong>GitHub:</strong> <a href="https://github.com/RodrigoBJJ94" target="_blank" rel="noreferrer" className="contact-mobile">https://github.com/RodrigoBJJ94</a></p>
