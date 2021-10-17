1ebd321f12e3f16e7828ae74a97ca6ec3c7df23f
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-17 05:06:14 -0300

    Version 1.21

--- src/components/Mobile/MobileMain.js
+++ src/components/Mobile/MobileMain.js
@@ -4,6 +4,7 @@ export default function MobileMain() {
     return (
         <>
             <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Tic Tac Toe</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.headsOrTailsCaraOuCoroa" target="_blank" rel="noreferrer">Cities Weather</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculator</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculator of IMC</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.gasolinaOuEtanol" target="_blank" rel="noreferrer">Gasoline or Ethanol</a></h4>
