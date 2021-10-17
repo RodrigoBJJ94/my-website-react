b5e9d7101d53426a2e6d75e5d6028151596bdf55
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-17 17:10:19 -0300

    Version 1.22

--- src/components/Contact/ContactMain.js
+++ src/components/Contact/ContactMain.js
@@ -4,7 +4,7 @@ export default function ContactMain() {
     return (
         <>
             <p><strong>WhatsApp:</strong>  +55 51 992191122</p>
-            <p><strong>LinkedIn:</strong><br /> <a href="https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/" target="_blank" rel="noreferrer" className="contact-mobile-linkedin">https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/</a></p>
+            <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/rodrigo-brentano-26a7ba204/" target="_blank" rel="noreferrer" className="contact-mobile" >...rodrigo-brentano-26a7ba204</a></p>
             <p><strong>Email:</strong> rodrigobjj24@gmail.com</p>
             <p><strong>GitHub:</strong> <a href="https://github.com/RodrigoBJJ94" target="_blank" rel="noreferrer" className="contact-mobile">https://github.com/RodrigoBJJ94</a></p>
         </>
