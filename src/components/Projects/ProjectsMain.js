e9bee50b424e090dec984eadbc45c89ab0b37fc4
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-20 21:18:43 -0300

    Version 1.25

--- src/components/Projects/ProjectsMain.js
+++ src/components/Projects/ProjectsMain.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default function ProjectsMain() {
     return (
         <>
-            <h4><a href="https://rodrigobrentano-annotations-maker.netlify.app/" target="_blank" rel="noreferrer" >Annotations Maker</a></h4>
-            <h4><a href="https://rodrigobrentano-pomodoro.netlify.app" target="_blank" rel="noreferrer" >Pomodoro</a></h4>
+            <h4><a href="https://rodrigobrentano-annotations-maker.netlify.app" target="_blank" rel="noreferrer" >Annotations Maker</a></h4>
+            <h4><a href="https://rodrigobrentano-currency-converter.netlify.app" target="_blank" rel="noreferrer" >Currency Converter</a></h4>
             <h4><a href="https://rodrigobrentano-calculator.netlify.app" target="_blank" rel="noreferrer" >Calculator</a></h4>
+            <h4><a href="https://rodrigobrentano-pomodoro.netlify.app" target="_blank" rel="noreferrer" >Pomodoro</a></h4>
             <h4><a href="https://rodrigobrentano-calculator-of-bmi.netlify.app" target="_blank" rel="noreferrer" >Calculator of BMI</a></h4>
             <h4><a href="https://rodrigobrentano-lista-de-tarefas.netlify.app" target="_blank" rel="noreferrer" >To-do List</a></h4>
             <h4><a href="https://rodrigobrentano-timer.netlify.app" target="_blank" rel="noreferrer" >Timer</a></h4>
