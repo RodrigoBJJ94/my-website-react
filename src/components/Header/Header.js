25f574286583f83878f682afbe2c116380ff728a
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-20 21:48:36 -0300

    Version 1.26

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <aside className="menu white-background">
             <div className="main-content menu-content">
-                <h1><a href="#home">HOME</a></h1>
+                <h1><a href="#home" className="home-a">HOME</a></h1>
                 <nav>
                     <ul>
                         <li><a href="#mobile">Mobile</a></li>
