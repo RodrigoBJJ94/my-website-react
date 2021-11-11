df1142d9528a8199177bf66f58f6ea43108d1a39
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-11-11 14:00:16 -0300

    Version 1.28

--- src/components/Projects/ProjectsMain.js
+++ src/components/Projects/ProjectsMain.js
@@ -8,6 +8,7 @@ export default function ProjectsMain() {
             <h4><a href="https://rodrigobrentano-calculator.netlify.app" target="_blank" rel="noreferrer" >Calculator</a></h4>
             <h4><a href="https://rodrigobrentano-pomodoro.netlify.app" target="_blank" rel="noreferrer" >Pomodoro</a></h4>
             <h4><a href="https://rodrigobrentano-calculator-of-bmi.netlify.app" target="_blank" rel="noreferrer" >Calculator of BMI</a></h4>
+            <h4><a href="https://rodrigobrentano-text-case-converter.netlify.app/" target="_blank" rel="noreferrer" >Text Case Converter</a></h4>
             <h4><a href="https://rodrigobrentano-lista-de-tarefas.netlify.app" target="_blank" rel="noreferrer" >To-do List</a></h4>
             <h4><a href="https://rodrigobrentano-timer.netlify.app" target="_blank" rel="noreferrer" >Timer</a></h4>
         </>
