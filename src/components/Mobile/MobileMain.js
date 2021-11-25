8f15b6ce3fdc769895f91e8a08e5fb43e0204307
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-11-25 00:59:00 -0300

    Version 1.29

--- src/components/Mobile/MobileMain.js
+++ src/components/Mobile/MobileMain.js
@@ -5,6 +5,7 @@ export default function MobileMain() {
         <>
             <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Tic Tac Toe</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.weathercitiesclima" target="_blank" rel="noreferrer">Cities Weather</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.conversormedidas" target="_blank" rel="noreferrer">Measure Converter</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculator</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calories" target="_blank" rel="noreferrer">Calorie Counter</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculator of IMC</a></h4>
