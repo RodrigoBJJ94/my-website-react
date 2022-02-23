e403d79499afeb7acb1df37d83622c9f5231edb2
Rodrigo Brentano <rodrigobjj24@gmail.com>
2022-02-23 20:47:20 -0300

    Version 1.32

--- src/components/Mobile/MobileMain.js
+++ src/components/Mobile/MobileMain.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function MobileMain() {
     return (
         <>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.animeApp" target="_blank" rel="noreferrer">Anime App</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Tic Tac Toe</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.weathercitiesclima" target="_blank" rel="noreferrer">Cities Weather</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.conversormedidas" target="_blank" rel="noreferrer">Measure Converter</a></h4>
